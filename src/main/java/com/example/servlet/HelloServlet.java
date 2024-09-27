9f785c09910e3b0a9f121eb904bba34acd487773
YahorNaumovich <egor.n.naumovich@gmail.com>
2024-09-27 19:04:26 +0300

    version 1

--- src/main/java/com/example/servlet/HelloServlet.java
+++ src/main/java/com/example/servlet/HelloServlet.java
@@ -12,6 +12,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>This is the <b>first</b> (not really) version of the app!</h1>");
+        response.getWriter().println("<p>This is the <b>first</b> (not really) version of the app!</p>");
     }
 }
